59686c11907ed9416523055f4bf7736509e5946c
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-12-20 20:12:31 -0300

    fix: create election with duplicated candidates

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,5 +1,5 @@
 class Candidate < ApplicationRecord
   belongs_to :election
 
-  validates :name, presence: true, uniqueness: { scope: :election_id, case_sensitive: false, message: "must be unique within the same election" }
+  validates :name, presence: true
 end
--- app/models/election.rb
+++ app/models/election.rb
@@ -4,11 +4,21 @@ class Election < ApplicationRecord
 
   validates :title, presence: true
   validate :must_have_at_least_two_candidates
+  validate :must_have_unique_candidates
 
 private
   def must_have_at_least_two_candidates
     if candidates.size < 2
       errors.add(:candidates, "must have at least two associated")
+    end
+  end
+
+  def must_have_unique_candidates
+    candidate_names = candidates.map { |candidate| candidate.name.downcase }
+    candidate_names = candidate_names.filter { |name| name.present? }
+    duplicated_names = candidate_names.select { |name| candidate_names.count(name) > 1 }
+    if duplicated_names.any?
+      errors.add(:candidates, "must be unique within the same election")
     end
   end
 end
