99db0e6c2f9e3d76062d0a888120ebdd3ce1971e
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-12-26 17:38:12 -0300

    feat: add election association to vote model and validate open elections

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,13 @@
 class Vote < ApplicationRecord
   belongs_to :candidate
+  has_one :election, through: :candidate
+
+  validate :must_have_open_election
+
+private
+  def must_have_open_election
+    if election.closed?
+      errors.add(:election, "must be open")
+    end
+  end
 end
